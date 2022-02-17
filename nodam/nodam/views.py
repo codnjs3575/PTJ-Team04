b876d01ae91857e2dbff265c2a0edd4635efab24
codnjs3575 <codnjs3575@naver.com>
2022-02-18 00:41:53 +0900

    add all

--- nodam/nodam/views.py
+++ nodam/nodam/views.py
@@ -1,9 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import redirect, render
+from .models import SignUp
 
 def index(requests): return render(requests,'index.html')
-def signup(requests): return render(requests,'signup.html')
-def login(requests): return render(requests,'login.html')
 def dashboard(requests): return render(requests,'dashboard.html')
 def smokingmap(requests): return render(requests,'map.html')
 def clinic(requests): return render(requests,'clinic.html')
 def community(requests): return render(requests,'community.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    else:
+        userID = request.POST['userID']
+        userPW = request.POST['userPW1']
+        userName = request.POST['userName']
+        userbirthDay = request.POST['userbirthDay']
+        userGender = request.POST['userGender']
+        userEmail = request.POST['userEmail']
+        userPhone = request.POST['userPhone']
+        userAttitude = request.POST['userAttitude']
+        startDay = request.POST['startDay']
+
+        SignUp.objects.create(userID=userID, userPW=make_password(userPW), userName=userName,
+                            userbirthDay=userbirthDay, userEmail=userEmail,userGender=userGender,
+                            userPhone=userPhone,userAttitude=userAttitude,startDay=startDay)
+        return redirect('/')
+
+def login(request):
+    if request.method == 'POST':
+        userID = request.POST['username']
+        userPW = request.POST['password']
+        if userID and userPW: 
+            user = SignUp.objects.get(userID=userID)
+
+            if check_password(userPW , user.userPW):
+                request.session['username'] = user.userID
+                return redirect('/')
+            else:
+                return redirect('/login')
+    else: 
+        return render(request,'login.html')
+
+def logout(requests):
+    del requests.session['username']
+    return redirect('/')
